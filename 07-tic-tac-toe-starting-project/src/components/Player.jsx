c44140412b4a84d1127dfe4d139679c6f2374605
psachdeva0108 <psachdeva0108@gmail.com>
2026-06-17 04:05:27 +0530

    final files added

--- 07-tic-tac-toe-starting-project/src/components/Player.jsx
+++ 07-tic-tac-toe-starting-project/src/components/Player.jsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-export default function Player({ initialName, symbol }) {
+export default function Player({ initialName, symbol, isActive }) {
     const [playerName, setPlayerName] = useState(initialName);
      const [isEditing, setIsEditing] = useState(false);
      function handleEditClick() {
@@ -15,7 +15,7 @@ export default function Player({ initialName, symbol }) {
         // btncaption = "save";
      }
     return (
-        <li>
+        <li className={isActive ? 'active' : undefined}>
             <span className="player">
                 {editableplayerName}
                 <span className="player-symbol">{symbol}</span>
