73672ebb04b068ef7221ba1ce5f6e7950e77de73
psachdeva0108 <psachdeva0108@gmail.com>
2026-06-23 03:40:57 +0530

    new folder

--- starting-project-styling/src/components/Header.jsx
+++ starting-project-styling/src/components/Header.jsx
@@ -1,11 +1,13 @@
 import logo from '../assets/logo.png';
 
+import {styled} from 'styled-components'    
+            
 export default function Header() {
   return (
-    <header>
-      <img src={logo} alt="A canvas" />
-      <h1>ReactArt</h1>
-      <p>A community of artists and art-lovers.</p>
+    <header className='flex flex-col items-center mt-8 mb-16'>
+      <img src={logo} alt="A canvas" className='object-contain mb-8 w-44 h-44'/>
+      <h1 className='text-4xl font-semibold tracking-widest text-center uppercase text-amber-800 font-title'>ReactArt</h1>
+      <p className='text-stone-500'>A community of artists and art-lovers.</p>
     </header>
   );
 }
