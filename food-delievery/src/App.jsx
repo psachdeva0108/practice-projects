2ab0540b8bd023ec37d6a144a7c887b7f92bfbec
psachdeva0108 <psachdeva0108@gmail.com>
2026-07-24 02:04:34 +0530

    new files

--- food-delievery/src/App.jsx
+++ food-delievery/src/App.jsx
@@ -1,10 +1,20 @@
+import Cart from './components/Cart.jsx';
+import Checkout from './components/Checkout.jsx';
+import Header from './components/Header.jsx';
+import Meals from './components/Meals.jsx';
+import { CartContextProvider } from './store/CartContext.jsx';
+import { UserProgressContextProvider } from './store/UserProgressContext.jsx';
+
 function App() {
   return (
-    <>
-      <h1>You got this 💪</h1>
-      <p>Stuck? Not sure how to proceed?</p>
-      <p>Don't worry - we've all been there. Let's build it together!</p>
-    </>
+    <UserProgressContextProvider>
+      <CartContextProvider>
+        <Header />
+        <Meals />
+        <Cart />
+        <Checkout />
+      </CartContextProvider>
+    </UserProgressContextProvider>
   );
 }
 
